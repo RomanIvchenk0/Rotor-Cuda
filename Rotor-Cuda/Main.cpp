ad025d5158b0b974e7cd4c5dd651dd813a4b38ca
Roman I <4783338+RomanIvchenk0@users.noreply.github.com>
2025-07-10 22:51:36 +0200

    Update Main.cpp

--- Rotor-Cuda/Main.cpp
+++ Rotor-Cuda/Main.cpp
@@ -197,6 +197,50 @@ int main(int argc, char** argv)
 	// Global Init
 	Timer::Init();
 	rseed(Timer::getSeed32());
+	#if defined(_WIN32) || defined(_WIN64)
+	struct console
+	    {
+	        console(unsigned width, unsigned height)
+		{
+			SMALL_RECT r;
+			COORD      c;
+			hConOut = GetStdHandle(STD_OUTPUT_HANDLE);
+			if (!GetConsoleScreenBufferInfo(hConOut, &csbi))
+				throw runtime_error("  You must be attached to a human.");
+
+			r.Left =
+				r.Top = 0;
+			r.Right = width - 1;
+			r.Bottom = height - 1;
+			SetConsoleWindowInfo(hConOut, TRUE, &r);
+
+			c.X = width;
+			c.Y = height;
+			SetConsoleScreenBufferSize(hConOut, c);
+		}
+
+		~console()
+		{
+			SetConsoleTextAttribute(hConOut, csbi.wAttributes);
+			SetConsoleScreenBufferSize(hConOut, csbi.dwSize);
+			SetConsoleWindowInfo(hConOut, TRUE, &csbi.srWindow);
+		}
+
+		void color(WORD color = 0x07)
+		{
+			SetConsoleTextAttribute(hConOut, color);
+		}
+
+		HANDLE                     hConOut;
+		CONSOLE_SCREEN_BUFFER_INFO csbi;
+	    };
+	#else
+	struct console
+	{
+	    console(unsigned width, unsigned height) {}
+	    void color(int /*c*/ = 7) {}
+	};
+	#endif
 	struct console
 	{
 		console(unsigned width, unsigned height)
